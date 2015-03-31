30b6be6c5d06628de0d2a3d53d43c52a9cb87b50
Michal Kuchta <niximor@gmail.com>
2015-03-31 13:51:01 +0200

    File wrapper

---
+++ include/gcm/io/file.h
@@ -0,0 +1,65 @@
+/**
+ * Copyright 2014 Michal Kuchta <niximor@gmail.com>
+ *
+ * This file is part of GCM::AppSrv.
+ *
+ * GCM::AppSrv is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * GCM::AppSrv is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+ * or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+ * for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with GCM::AppSrv. If not, see http://www.gnu.org/licenses/.
+ *
+ * @author Michal Kuchta <niximor@gmail.com>
+ * @date 2015-03-13
+ *
+ */
+
+#pragma once
+
+#include <stdio.h>
+
+#include "exception.h"
+
+namespace gcm {
+namespace io {
+
+/**
+ * Class that wraps standard FILE * object and manages it automatically.
+ */
+class File {
+public:
+    File(const std::string &file_name, const char *mode): f(::fopen(file_name.c_str(), mode)) {
+        if (!f) {
+            throw gcm::io::IOException(errno);
+        }
+    }
+
+    File(const File &other) = delete;
+
+    File(File &&other): f(other.f) {
+        other.f = nullptr;
+    }
+
+    operator FILE *() {
+        return f;
+    }
+
+    ~File() {
+        if (f) {
+            ::fclose(f);
+        }
+    }
+
+protected:
+    FILE *f;
+};
+
+} // namespace io
+} // namespace gcm
